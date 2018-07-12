e06ffaa71dfc3f9a58890f17dda28661faf3beff
NareshKeerapatla <39722428+NareshKeerapatla@users.noreply.github.com>
2018-07-12 11:15:31 +0530

    Update models.py

--- RApp/models.py
+++ RApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Reg(models.Model):
     User=models.CharField(max_length=30)
-    Pwd=models.CharField(max_length=10)
+    Pwd=models.CharField(max_length=20)
     Fname=models.CharField(max_length=20)
     Lname=models.CharField(max_length=20)
     Dob=models.DateField()
